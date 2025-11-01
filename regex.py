47a5d4836cf7f42321bed4ed8c88ff2a63bc56b6
JakirHossain25 <sarkerjakariya5@gmail.com>
2025-11-01 23:59:37 +0600

    two  type of egulaar expretion

--- regex.py
+++ regex.py
@@ -1,5 +1,11 @@
 import re
 text="Email you feedback: book@gmail.com book thank you"
-result= re.findall(r"(\w+@\w+.\w+)",text)
+result= re.findall(r"(\w+@\w+\.\w+)",text)
 print(result)
+
+
+text="Email you feedback: book@gmail.com book thank you"
+result1= re.findall(r"(\w+@\w+[.]\w+)",text)
+print(result1)
+
 
