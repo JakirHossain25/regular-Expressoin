8af8152728ca435b2cbb8d6028e695d1c2c3890a
JakirHossain25 <sarkerjakariya5@gmail.com>
2025-11-03 00:05:48 +0600

    Regular expration class 2

--- regex.py
+++ regex.py
@@ -1,11 +1,16 @@
 import re
-text="Email you feedback: book@gmail.com book thank you"
-result= re.findall(r"(\w+@\w+\.\w+)",text)
+# text="Email yoi feedback here : book@subeen.com py.book@@subeen.com book_py@subeen.com thank you"
+# result= re.findall(r"(\w+@\w+[.]\w+)",text)
+# print(result)
+
+
+# tx="Email yoi feedback here : book@subeen.com py.book@@subeen.com book_py@subeen.com thank you"
+# result= re.findall(r"[.\w]+@\w+[.]\w+",tx)
+# print(result)
+
+
+
+tx="book At subeen.com, book At subeen.com,book (at) subben dot com,book [at] subeen [dot] com"
+result= re.sub(r"\s+[\[]*\s*at\s*[\)\]]*\s+",'@',tx,flags=re.I)
 print(result)
-
-
-text="Email you feedback: book@gmail.com book thank you"
-result1= re.findall(r"(\w+@\w+[.]\w+)",text)
-print(result1)
-
 
